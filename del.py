3fe9b00221de8e76fe9847e26c92549e1971d28f
Karthik Kanagond <01fe25bca308@kletech.ac.in>
2026-09-04 10:19:21 +0530

    save the file to third

---
+++ del.py
@@ -0,0 +1,8 @@
+a = 10;
+b =40;
+print("addition:",a+b);
+print("subtraction:",a-b);
+print("multiplication:",a*b);
+print("division:",a/b);
+print("All operations completed.")
+print("average:",(a+b)/2);
